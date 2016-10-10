4284ac45522f166c3ca2f759cc48f0c54ed436a8
Jason Bland <jtbland@gmail.com>
2016-10-09 22:44:47 -0700

    Create header component and add to details and search component

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -1,14 +1,25 @@
 const React = require('react');
+const Header = require('./Header');
 
 class Details extends React.Component {
   render () {
+    const params = this.props.params || {};
+    const { title, description, pubDate, stationId } = params;
+
     return (
       <div className='container'>
-        <pre>
-          <code>
-            {JSON.stringify(this.props.params, null, 4)}
-          </code>
-        </pre>
+        <Header />
+        <div className='buoy-info'>
+          <h1 className='buoy-title'>{title}</h1>
+          <h2 className='buoy-year'>{stationId}</h2>
+          <h2 className='buoy-year'>{pubDate}</h2>
+          <img className='buoy-image' src={`client/images/buoy.jpg`} />
+          <ul className='buoy-description'>
+            {description.map((detail) => (
+              <li dangerouslySetInnerHTML={{__html: detail}} key={detail} />
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
