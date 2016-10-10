fd84f86b01f9be5a8c8a735a01967fc4e62d316c
Jason Bland <jtbland@gmail.com>
2016-10-09 23:12:13 -0700

    Add link to buoycard for details page.

--- client/components/BuoyCard.jsx
+++ client/components/BuoyCard.jsx
@@ -1,18 +1,21 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const BuoyCard = (props) => (
-  <div className='buoy-card'>
-    <img src={`client/images/buoy.jpg`} className='buoy-card-img' />
-    <div className='buoy-card-text'>
-      <h3 className='buoy-card-title'>{props.title}</h3>
-      <h4 className='buoy-card-year'>{props.pubDate}</h4>
-      <ul className='buoy-card-description'>
-        {props.description.map((detail) => (
-          <li dangerouslySetInnerHTML={{__html: detail}} key={detail} />
-        ))}
-      </ul>
+  <Link to={`/details/${props.stationId}`}>
+    <div className='buoy-card'>
+      <img src={`client/images/buoy.jpg`} className='buoy-card-img' />
+      <div className='buoy-card-text'>
+        <h3 className='buoy-card-title'>{props.title}</h3>
+        <h4 className='buoy-card-year'>{props.pubDate}</h4>
+        <ul className='buoy-card-description'>
+          {props.description.map((detail) => (
+            <li dangerouslySetInnerHTML={{__html: detail}} key={detail} />
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
+  </Link>
 );
 
 const { string, array } = React.PropTypes;
@@ -20,7 +23,8 @@ const { string, array } = React.PropTypes;
 BuoyCard.propTypes = {
   title: string.isRequired,
   pubDate: string.isRequired,
-  description: array.isRequired
+  description: array.isRequired,
+  stationId: string.isRequired
 };
 
 module.exports = BuoyCard;
